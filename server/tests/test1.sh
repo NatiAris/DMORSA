645af70f86be04e3892f004ce6d881b90958523b
NatiAris <NatiAris0@gmail.com>
2017-03-03 16:08:08 +0300

    Added simple test script for get requests & modified script for post request testing

---
+++ server/tests/test1.sh
@@ -0,0 +1,18 @@
+# Creates a session with constant session_id and leg_id, using all 4 POST methods, pretty prints the results and clears sessions collection.
+
+# Create a session
+curl -X POST --data @testset1/cs.json http://localhost:3467/ --header "Content-Type:application/json"
+# Create a leg
+curl -X POST --data @testset1/cl.json http://localhost:3467/ --header "Content-Type:application/json"
+# Update the session
+curl -X POST --data @testset1/ul.json http://localhost:3467/ --header "Content-Type:application/json"
+# Update the leg
+curl -X POST --data @testset1/us.json http://localhost:3467/ --header "Content-Type:application/json"
+# Get the data (commented since field names changed)
+# curl "http://localhost:3467/?request_type=time_only&over_last=100000"
+# mongo < get_sessions.js
+# Clear sessions collection
+# mongo < purge.js
+
+# Get sessions AND purge
+mongo < get_and_purge.js
---
+++ server/tests/testset2/purge.js
@@ -0,0 +1,3 @@
+use asl
+db.sessions.remove({"_id": 200001})
+db.accounts.remove({"_id": 20001})
