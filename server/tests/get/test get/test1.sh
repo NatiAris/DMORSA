47ea26d572e7dae50696a0466b671f73577041c0
NatiAris <NatiAris0@gmail.com>
2017-03-16 14:56:50 +0300

    Change permissions

--- server/tests/get/test get/test1.sh
+++ server/tests/get/test get/test1.sh
@@ -1,1 +1,1 @@
-curl "http://localhost:3467/?request_type=phones_time&phone_ids=10,11&over_last=24" > response_t1.txt
+curl "http://localhost:3467/?request_type=phones_time&phone_ids=10,11&over_last=24" # > response_t1.txt
