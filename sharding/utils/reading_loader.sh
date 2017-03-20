2f2d9732d4d1ec9c006095b3e65c58158528efb9
Mikova <mikovaval@gmail.com>
2017-03-20 18:46:57 +0500

    Tests for reading load

--- sharding/utils/reading_loader.sh
+++ sharding/utils/reading_loader.sh
@@ -1,20 +1,34 @@
-# Written by Mikova Valentina and Salavat Garifullin
+# Written by Valentina Mikova and Salavat Garifullin
 # Generating get requests for profiling. It takes into account the different frequency of requests.
 
 from_=$(shuf -i 100-999 -n 1)
 q=$(shuf -i 0-10000 -n 1)
+
+h24=0.66667
+h72=0.22933
+d7=0.066666
+d14=0.03333
+m1=0.003333
+m3=0.000667
+
+h24=$(echo "scale=5;$h24*100000" |bc)
+h72=$(echo "scale=5;$h24+$h72*100000" |bc)
+d7=$(echo "scale=5;$h72+$d7*100000" |bc)
+d14=$(echo "scale=5;$d7+$d14*100000" |bc)
+m1=$(echo "scale=5;$d14+$m1*100000" |bc)
+
 now=$[$(date +%s)*1000]
 
-(( ($q < 6666) && (dt=86400000) )) || 
-(( ($q < 8966) && (dt=259200000) )) || 
-(( ($q < 9636) && (dt=604800000) )) || 
-(( ($q < 9966) && (dt=1209600000) )) || 
-(( ($q < 9999) && (dt=2419200000) )) || 
-                 ((dt=7257600000))
-#24h-3m
+(( ($q < $h24) && (dt=86400000)  )) ||    #24h,72h,7d,14d,1m,4m
+(( ($q < $h72) && (dt=259200000) )) || 
+(( ($q < $d7)  && (dt=604800000) )) || 
+(( ($q < $d14) && (dt=1209600000))) || 
+(( ($q < $m1)  && (dt=2592000000))) || 
+                  ((dt=10368000000))
+
 echo $q
-date -u -d @$[$[$now-$dt]/1000]
-date -u -d @$[$now/1000]
+date -u -d @$[$[$now-$dt]/1000] +%Y-%m-%d:%H:%M:%S
+date -u -d @$[$now/1000] +%Y-%m-%d:%H:%M:%S
 
 request='db.legs.find({created:{$gte: new Date('$[$now-$dt]'),$lte: new Date('$[$now]')}},{_id:0,created:1}).hint({created:1}).sort({created:1}).limit(10)'
 mongo asl --port 40000 --quiet --norc --eval="$request"
---
+++ sharding/utils/test/reading_load2.sh
@@ -0,0 +1,35 @@
+# Written by Valentina Mikova
+# It sends get requests and should create an approximately uniform load on 4 existing shards: 
+# 2 days, 2 weeks, 1 month, 4 months.
+# The load on the individual shards
+
+from_=$(shuf -i 100-999 -n 1)
+q=$(shuf -i 0-10000 -n 1)
+h72=0.75089
+w2=0.12515
+m1=0.10727
+m4=0.01669
+
+b1=$[2*24*3600*1000]	# 2 days
+b2=$[14*24*3600*1000]	# 2 weeks
+b3=$[30*24*3600*1000]	# 1 month
+b4=$[120*24*3600*1000]	# 4 months
+
+h72=$(echo "scale=5;$h72*100000" |bc)
+w2=$(echo "scale=5;$h72+$w2*100000" |bc)
+m1=$(echo "scale=5;$w2+$m1*100000" |bc)
+m4=$(echo "scale=5;$m1+$m4*100000" |bc)
+
+now=$[$(date +%s)*1000]
+
+(( ($q < $h72) && (dt=$(shuf -i 3600000-$b1 -n 1))  && (delta=0)   )) || 
+(( ($q < $w2)  && (dt=$(shuf -i $[$b1+1]-$b2 -n 1)) && (delta=$b1) )) || 
+(( ($q < $m1)  && (dt=$(shuf -i $[$b2+1]-$b3 -n 1)) && (delta=$b2) )) || 
+(( ($q < $m4)  && (dt=$(shuf -i $[$b3+1]-$b4 -n 1)) && (delta=$b3) ))
+
+echo $q
+date -u -d @$[$[$now-$delta-$dt]/1000] +%Y-%m-%d:%H:%M:%S
+date -u -d @$[$[$now-$delta]/1000] +%Y-%m-%d:%H:%M:%S
+
+request='db.legs.find({created:{$gte: new Date('$[$now-$delta-$dt]'),$lte: new Date('$[$now-$delta]')}},{_id:0,created:1}).hint({created:1}).sort({created:1}).limit(10)'
+mongo asl --port 40000 --quiet --norc --eval="$request"
